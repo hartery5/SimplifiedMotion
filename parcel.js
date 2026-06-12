c7f9c1144ca2e5b3fe7b7d5b48c74350504b26f3
hartery5 <78227345+hartery5@users.noreply.github.com>
2026-06-12 14:05:05 -0300

    Refactor physics and initialize methods with speedField

--- parcel.js
+++ parcel.js
@@ -3,20 +3,22 @@ class parcel {
     this.x = x;
     this.y = y;
     
-    this.vx = 0;
-    this.vy = 0;
+    this.vx = 0.0;
+    this.vy = 0.0;
     
     this.hist = [];
     
-    this.cor = 0.05;
-    this.rho = 4;
+    this.cor = 0.1;
+    this.rho = 2.0;
+    this.mu = 0.01;
     this.dt = 0.25;
 
     this.move = false;
     this.kill = false;
+    this.age = 0;
   }
   
-  physics(pressureField){
+  physics(pressureField,speedField){
     let posX = round(this.x/fac)+1;
     let posY = round(this.y/fac)+1;
     
@@ -24,12 +26,19 @@ class parcel {
       let dpdx = pressureField[posX+1][posY].V-pressureField[posX-1][posY].V;
       let dpdy = pressureField[posX][posY+1].V-pressureField[posX][posY-1].V;
       
-      let ax = -this.cor*this.vy - (1/this.rho)*dpdx;
-      let ay = this.cor*this.vx - (1/this.rho)*dpdy;
+      let ax = -(1/this.rho)*dpdx;
+      let ay = -(1/this.rho)*dpdy;
 
-      this.x += this.vx*this.dt;
-      this.y += this.vy*this.dt;
-      
+      if (frictionBox.checked()==true){
+        ax -= this.mu*this.vx;
+        ay -= this.mu*this.vy;
+      }
+
+      if (coriolisBox.checked()==true){
+        ax -= this.cor*this.vy;
+        ay += this.cor*this.vx;
+      }
+
       // Simplified Eq's of Motion
       if (motionType.selected()=='Real Motion'){
         this.x += this.vx*this.dt;
@@ -45,13 +54,17 @@ class parcel {
         this.vx = (1/(this.cor*this.rho))*dpdy;
         this.vy = -(1/(this.cor*this.rho))*dpdx;
       }
-      
+
+      let s = sqrt(pow(this.vx,2)+pow(this.vy,2));
+      speedField[posX][posY].V = speedField[posX][posY].V*(this.age-1)/this.age + s/this.age;
+
+      this.age+=1
     } else {
       this.kill = true;
     }
   }
   
-  initialize(pressureField){
+  initialize(pressureField,speedField){
     let posX = round(this.x/fac);
     let posY = round(this.y/fac);
     
@@ -61,9 +74,12 @@ class parcel {
       
       this.vx = (1/(this.cor*this.rho))*dpdy;
       this.vy = -(1/(this.cor*this.rho))*dpdx;
+
+      let s = sqrt(pow(this.vx,2)+pow(this.vy,2));
+      speedField[posX][posY].V = s;
       
       this.move = true;
-      
+      this.age +=1;
     } else {
       this.kill = true;
     }
